267703b7feb976145e4f02036ea4e50488879382
guinuxbr <guinuxbr@gmail.com>
2020-08-29 19:30:01 -0300

    Chuck Norris is to heavy for the web

--- js/script.js
+++ js/script.js
@@ -1,29 +1,35 @@
 const jokeContainer = document.querySelector("#joke-container");
 const jokeText = document.querySelector("#joke");
-const twitterBtn = document.querySelector("#twitter");
-const newJokeBtn = document.querySelector("#new-joke");
+const twitterBtn = document.querySelector("#btn-twitter");
+const newJokeBtn = document.querySelector("#btn-new-joke");
 const loader = document.querySelector("#loader");
 const inputSearch = document.querySelector("#input-search");
 const message = document.querySelector(".message");
+const inputContainer = document.querySelector(".input-container");
+const clearBtn = document.querySelector("#btn-clear");
 
 // Event Listeners
 newJokeBtn.addEventListener("click", searchJoke);
 twitterBtn.addEventListener("click", tweetJoke);
-
+clearBtn.addEventListener("click", () => {
+  inputSearch.value = "";
+});
 
 // Show Loading
 function showLoadingSpinner() {
-  loader.hidden = false;
-  jokeContainer.hidden = true;
-  message.hidden = true;
+  loader.style.display = "block";
+  jokeContainer.style.display = "none";
+  inputContainer.style.display = "none";
+  message.style.display = "none";
 }
 
 // Hide Loading
 function hideLoadingSpinner() {
   if (!loader.hidden) {
-    jokeContainer.hidden = false;
-    message.hidden = false;
-    loader.hidden = true;
+    jokeContainer.style.display = "flex";
+    message.style.display = "block";
+    inputContainer.style.display = "block";
+    loader.style.display = "none";
   }
 }
 
@@ -44,7 +50,6 @@ async function getJoke() {
     jokeText.innerText = data.value;
     // Stop Loader, Show the Joke
     hideLoadingSpinner();
-
   } catch (error) {
     message.innerText = `There was an error. Try again in a few seconds`;
   }
@@ -60,32 +65,37 @@ function tweetJoke() {
 // Searching for a joke
 async function searchJoke() {
   showLoadingSpinner();
-  const apiSearchURL = "https://api.chucknorris.io/jokes/search?query="
 
   if (inputSearch.value === "") {
     getJoke();
   } else {
-    try {
-      const response = await fetch(`${apiSearchURL}${inputSearch.value}`);
-      const data = await response.json();
-      const totalOfJokes = data.total;
-      const randomJoke = Math.floor(Math.random() * totalOfJokes);
-      
-      // Showing the total of found jokes
-      message.innerText = `We found ${totalOfJokes} jokes which contains the term "${inputSearch.value}".`;
-      
-      // Resize font if joke is long
-      if (data.result[randomJoke].value.length > 80) {
-        jokeText.classList.add("long-joke");
-      } else {
-        jokeText.classList.remove("long-joke");
-      }
-      jokeText.innerText = data.result[randomJoke].value;
+    const apiSearchURL = "https://api.chucknorris.io/jokes/search?query=";
+    const response = await fetch(`${apiSearchURL}${inputSearch.value}`);
+    const data = await response.json();
+    const totalOfJokes = data.total;
+    const randomJoke = Math.floor(Math.random() * totalOfJokes);
 
-      // Stop Loader, Show the Joke
+    if (totalOfJokes === 0) {
       hideLoadingSpinner();
-    } catch (error) {
-      message.innerText = `There was an error. Try again in a few seconds`;
+      message.innerText = `Am I joke for you? There is nothing which contains "${inputSearch.value}".`;
+    } else {
+      try {
+        // Showing the total of found jokes
+        message.innerText = `We found ${totalOfJokes} jokes which contains the term "${inputSearch.value}".`;
+
+        // Resize font if joke is long
+        if (data.result[randomJoke].value.length > 80) {
+          jokeText.classList.add("long-joke");
+        } else {
+          jokeText.classList.remove("long-joke");
+        }
+        jokeText.innerText = data.result[randomJoke].value;
+
+        // Stop Loader, Show the Joke
+        hideLoadingSpinner();
+      } catch (error) {
+        message.innerText = `There was an error. Try again in a few seconds`;
+      }
     }
   }
 }
